8aa6c05eb7b8604a54fb5e77369e6c47173cfe1a
eleniel-mocna <soukup.sam@gmail.com>
2021-07-20 21:26:24 +0200

    Fixed import in some cases - hopefully...

--- CellCNN/__init__.py
+++ CellCNN/__init__.py
@@ -2,6 +2,6 @@ from os.path import dirname, realpath
 import sys
 sys.path.insert(0, dirname(realpath(__file__)))
 
-import CellCNN.InputData as InputData
-import CellCNN.Dataset as Dataset
-import CellCNN.Models as Models
+from .InputData import *
+from .Dataset import *
+from .Models import *
