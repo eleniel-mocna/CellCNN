eb74f48fed655266364b8449d79df4ec36295cdc
Samuel Soukup <soukup.sam@gmail.com>
2021-08-18 11:18:37 +0200

    Added dropout, corrected save/load

--- CellCNN/Models.py
+++ CellCNN/Models.py
@@ -29,7 +29,8 @@ class CellCNN(Model):
                  k=25,
                  lr=0.01,
                  activation="relu",
-                 l1_weight=0.01):
+                 l1_weight=0.01,
+                 dropout = 0.25):
         """
         Build CellCNN model
 
@@ -53,6 +54,7 @@ class CellCNN(Model):
         self.lr = lr
         self.activation = activation
         self.l1_weight = l1_weight
+        self.dropout = dropout
         self._build_layers()
         self._compile()
         self.build(tuple(self.my_input_shape))
@@ -66,6 +68,7 @@ class CellCNN(Model):
     def _build_filter_layers(self):
         self.my_layers = []
         for i in range(len(self.conv)):
+            self.my_layers.append(Dropout(self.dropout))
             if self.conv[i] != 0:
                 self.my_layers.append(
                     Conv1D(filters=self.conv[i],
@@ -73,7 +76,7 @@ class CellCNN(Model):
                            activation=self.activation,
                            )
                 )
-                self.my_layers.append(Dropout(0.25))
+
         self.my_layers.append(L1Layer(self.l1_weight))
         self.my_layers.append(Lambda(self._select_top,
                                      output_shape=(1,),
@@ -254,7 +257,9 @@ class CellCNN(Model):
                 "conv": self.conv,
                 "k": self.k,
                 "lr": self.lr,
-                "activation": self.activation}
+                "activation": self.activation,
+                "l1_weight" : self.l1_weight,
+                "dropout" : self.dropout}
 
     def save(self, config_file, weights_file):
         json_config = self.get_config()
@@ -279,7 +284,9 @@ class CellCNN(Model):
                        conv=config["conv"],
                        k=config["k"],
                        lr=config["lr"],
-                       activation=config["activation"])
+                       activation=config["activation"],
+                       l1_weight=config["l1_weight"],
+                       dropout=config["dropout"])
 
 
 class SCellCNN(CellCNN):
